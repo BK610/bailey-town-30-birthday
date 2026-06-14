bb96f886308ecd346b7f911ea6772fb2a48c22c9
Bailey Kane <bailey.orion.kane@gmail.com>
2026-06-14 17:20:33 -0400

    add basic header and headshot

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import passportHeadshot from "$lib/assets/passport_headshot.jpg";
+</script>
+
+<h1>Welcome to Bailey Town</h1>
+<h2>It's my birthday. Happy birthday to us all</h2>
+<img id="passport-headshot" src={passportHeadshot} alt="Passport headshot" />
+
+<style>
+  #passport-headshot {
+    width: 100px;
+    height: 100px;
+    object-fit: cover;
+    border-radius: 50%;
+    border: 4px solid #009;
+  }
+</style>
